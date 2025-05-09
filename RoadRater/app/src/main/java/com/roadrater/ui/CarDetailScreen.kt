a3ce3ae412785cff9947305957da5a6e26e3e4dc
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2025-05-09 23:40:29 +1200

    lint

--- RoadRater/app/src/main/java/com/roadrater/ui/CarDetailScreen.kt
+++ RoadRater/app/src/main/java/com/roadrater/ui/CarDetailScreen.kt
@@ -22,8 +22,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -46,10 +48,6 @@ import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-
-
 
 class CarDetailScreen(val plate: String) : Screen {
 
@@ -185,13 +183,11 @@ class CarDetailScreen(val plate: String) : Screen {
                         onClick = { showDialog = true },
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(bottom = 16.dp)
+                            .padding(bottom = 16.dp),
                     ) {
                         Icon(imageVector = Icons.Outlined.Remove, contentDescription = "Remove")
                         Text("Remove from Watchlist", modifier = Modifier.padding(start = 8.dp))
                     }
-
-
 
                     Text(
                         text = "Reviews",
@@ -249,9 +245,8 @@ class CarDetailScreen(val plate: String) : Screen {
                     TextButton(onClick = { showDialog = false }) {
                         Text("Cancel")
                     }
-                }
+                },
             )
         }
-
     }
 }
--- RoadRater/app/src/main/java/com/roadrater/ui/newReviewScreen/NewReviewScreen.kt
+++ RoadRater/app/src/main/java/com/roadrater/ui/newReviewScreen/NewReviewScreen.kt
@@ -1,12 +1,27 @@
 package com.roadrater.ui.newReviewScreen
 
-import android.content.Context
 import android.util.Log
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -15,17 +30,14 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import com.google.android.gms.auth.api.identity.Identity
 import com.roadrater.auth.Auth
 import com.roadrater.database.entities.Review
-
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
-import org.koin.java.KoinJavaComponent.inject
 import java.time.Instant
 
 class NewReviewScreen(private val numberPlate: String) : Screen {
@@ -92,7 +104,7 @@ class NewReviewScreen(private val numberPlate: String) : Screen {
                         if (it.text.length <= 60) reviewTitle = it
                     },
                     label = { Text("Review Title (Max 60 characters):") },
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth(),
                 )
 
                 // REVIEW TEXT
@@ -127,7 +139,7 @@ class NewReviewScreen(private val numberPlate: String) : Screen {
                         rating = score,
                         title = reviewTitle.text,
                         description = commentText.text,
-                        createdAt = Instant.now().toString()
+                        createdAt = Instant.now().toString(),
                     )
 
                     CoroutineScope(Dispatchers.IO).launch {
@@ -147,8 +159,6 @@ class NewReviewScreen(private val numberPlate: String) : Screen {
                 }) {
                     Text("Submit review")
                 }
-
-
 
                 // MESSAGES
                 errorMessage?.let {
