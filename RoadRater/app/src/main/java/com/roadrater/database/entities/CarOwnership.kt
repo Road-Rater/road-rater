c975b7387ecc6f970c374df7356ee17c5ea9d8e8
Thomas <132997646+tomatotown51@users.noreply.github.com>
2025-06-10 11:08:37 +1200

    My Cars (#45)
    * Added My Cars tab to More screen.
    Added icon for My Cars tab
    Developed comments in MoreTab.kt
    
    * Remade MyCarsScreen.kt to follow rest of app's design language.
    
    Fixed text input field within MyCarsScreen.kt
    
    Added Screen Model info then separated it into its own class
    
    * Created CarOwnership.kt for querying and inserting into car_ownership table
    Added logic for car ownership
    Added checks to avoid car ownership duplication
    Added logging and error messaging relating to car ownership submission
    Removed hardcoded userID and implemented current userID
    Fixed bugs & added comments
    
    * -Added car cards to MyCarsScreen.kt
    -Updated MyCarsScreen.kt & MyCarsScreenModel.kt logic to allow de-registration of vehicles
    -Added onclick to cards to allow de-registration
    -Fixed bugs, added comments
    -Removed Herobrine
    
    * Cleaned up code
    
    * Removed hardcoded strings within MyCarsScreen.kt and MyCarsScreenModel.kt
    
    Added strings to strings.xml
    
    * Replaced license plate validation with ValidationUtils implementation within MyCarsScreenModel.kt
    
    * ./gradlew spotlessApply
    
    * -Fixed bugs

---
+++ RoadRater/app/src/main/java/com/roadrater/database/entities/CarOwnership.kt
@@ -0,0 +1,9 @@
+package com.roadrater.database.entities
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CarOwnership(
+    val number_plate: String,
+    val user_id: String? = null,
+)
